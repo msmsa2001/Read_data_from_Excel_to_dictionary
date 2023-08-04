10b4819eb561e4f8a12cb2facc3ba18bacb7ef0e
msmsa2001 <msmsa251001@gmail.com>
2023-08-04 19:19:28 +0530

    read Topic name and given topic id and chapter_id

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import openpyxl
+import json
 
 subject=[]
 chapter=[]
+topic=[]
 
 
 Excel=openpyxl.load_workbook("Excel_data.xlsx")
@@ -15,6 +17,9 @@ for i in range(len(sheets_name)):
 # print(subject)
 
 count = 1
+topid_id=1
+chapter_id=1
+len1=0
 for j in range(len(sheets_name)):
     current_sheet=Excel[sheets_name[j]]
     first_row_values = [cell.value for cell in current_sheet[1]]
@@ -22,7 +27,21 @@ for j in range(len(sheets_name)):
     chapter.extend(b)
     count += len(first_row_values) 
 
+    
+    for col in current_sheet.iter_cols(values_only=True):
+        new_col=[i for i in col[1:] if i is not None]
+        len1=len1+len(new_col)
+        c=[{'id':topid_id+id,'topic_name':new_col[i],'chapter_id':chapter_id} for id,i in enumerate(range(len(new_col)))]
+        topid_id += len(c)
+        topic.extend(c)
+        chapter_id=chapter_id+1
+
+
 # print(first_row_values)
 # print(current_sheet)
-print(chapter)
+# print(chapter)
+print(topic)
+# topic_pretty=json.dumps(topic,indent=4)
+# print(len1)
+# print(topic_pretty)
 
